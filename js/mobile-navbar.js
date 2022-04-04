767791576dbf7ef686cba0246244126bf3c4721c
gabeha <81102597+gabeha@users.noreply.github.com>
2022-04-04 14:40:43 +0200

    hide navbar on scroll

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -2,13 +2,20 @@ const navToggle = document.getElementById("menu-toggle");
 const links = document.querySelector(".navbar-links");
 const section = document.querySelector(".navbar-sections");
 const menuItem = document.querySelectorAll(".menu-item");
+const logo = document.querySelector(".logo");
 
 
+/**
+	Makes drop-down menu appear when navbar toggle is clicked
+**/
 navToggle.addEventListener('click', function() {
 	links.classList.toggle("mobile-nav");
 	section.classList.toggle("column-view");
 })
 
+/**
+	Makes drop-down menu disappear when link in navbar is clicked
+**/
 for (let i = 0; i<menuItem.length; i++) {
 	menuItem[i].addEventListener('click', function() {
 	links.classList.toggle("mobile-nav");
@@ -16,4 +23,12 @@ for (let i = 0; i<menuItem.length; i++) {
 })
 }
 
-
+/**
+	Makes drop-down menu disappear when name logo is clicked
+**/
+logo.addEventListener('click', function() {
+	if (links.classList.contains("mobile-nav")) {
+		links.classList.toggle("mobile-nav");
+		section.classList.toggle("column-view");
+	}
+})
--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,6 @@
+/**
+	Colorizes the selected link
+**/
 document.addEventListener("DOMContentLoaded", () => {
     const listOfLinks = document.querySelectorAll(".navbar-sections a[href^='#");
     listOfLinks.forEach(function (link) {
@@ -13,3 +16,27 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     })
 })
+
+/**
+	Automatically displays and hides navbar while scrolling
+**/
+document.addEventListener("DOMContentLoaded", function(){
+
+  el_autohide = document.querySelector('.autohide');
+
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+          let scroll_top = window.scrollY;
+         if(scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+          }
+          else {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    });
+  }
+});

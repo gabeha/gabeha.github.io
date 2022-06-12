5ebe58458076c1b53c4e2b3992252c6c555e6c2e
gabeha <81102597+gabeha@users.noreply.github.com>
2022-06-12 15:44:06 +0200

    naughty scroll fix mate

--- js/scroll.js
+++ js/scroll.js
@@ -25,10 +25,10 @@ document.addEventListener("DOMContentLoaded", function(){
   el_autohide = document.querySelector('.autohide');
 
   if(el_autohide){
-    var last_scroll_top = 300;
+    var last_scroll_top = 0;
     window.addEventListener('scroll', function() {
           let scroll_top = window.scrollY;
-          console.log(last_scroll_top)
+          console.log(scroll_top)
          if(scroll_top < last_scroll_top) {
               el_autohide.classList.remove('scrolled-down');
               el_autohide.classList.add('scrolled-up');
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function(){
                     section.classList.toggle("column-view");
                 }
           }
-          else {
+          else if (scroll_top > 10) {
               el_autohide.classList.remove('scrolled-up');
               el_autohide.classList.add('scrolled-down');
               if (links.classList.contains("mobile-nav")) {
